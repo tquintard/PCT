bb0bbc639b44742779396d98e09a8f7a0f16d36d
tquintard <95873239+tquintard@users.noreply.github.com>
2025-01-11 21:15:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
+# import streamlit as st
+# from streamlit_drawable_canvas import st_canvas
+# from PIL import Image
+# import os
+
+# # Charger une image par défaut
+# default_image_path = "test/Untitled.png"  # Remplacez par le chemin de votre image
+# if os.path.exists(default_image_path):
+#     default_image = Image.open(default_image_path).convert("RGBA")
+# else:
+#     st.error("L'image par défaut est introuvable. Vérifiez le chemin.")
+
+# # Dimensions fixes
+# resized_width, resized_height = 800, 600
+
+# # Afficher le canevas avec l'image
+# if 'default_image' in locals():
+#     canvas_result = st_canvas(
+#         stroke_width=2,
+#         stroke_color="#FF4B4B",
+#         background_image=default_image,  # Passez directement l'objet PIL ici
+#         update_streamlit=True,
+#         height=resized_height,
+#         width=resized_width,
+#         drawing_mode="freedraw",
+#         key="canvas_test",
+#     )
+
+#     # Vérifier les résultats
+#     if canvas_result and canvas_result.json_data:
+#         st.write("Points sélectionnés :", canvas_result.json_data["objects"])
+
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from PIL import Image
@@ -46,7 +78,6 @@ def main():
                 # Calculer la nouvelle hauteur de l'image pour garder les proportions
                 resized_width = int(col2_w) - 10
                 resized_height = int(resized_width * original_height / original_width)
-                st.write(f"Dimensions de l'image : {original_width} x {original_height}")
                 st.success("Image téléchargée avec succès.")
             except Exception as e:
                 st.error(f"Échec du téléchargement ou du traitement de l'image : {e}")
@@ -61,7 +92,7 @@ def main():
                 with col2:
                     # Afficher le canevas avec l'image
                     canvas_result = st_canvas(
-                        stroke_width=2,
+                        stroke_width=8,
                         stroke_color="#FF4B4B",
                         background_image=image,  # Passez directement l'objet PIL ici
                         update_streamlit=True,
