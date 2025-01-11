0febdba101e4c5f12f21b3f8fb740caa705cbd39
tquintard <95873239+tquintard@users.noreply.github.com>
2025-01-11 22:03:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,57 +22,49 @@ def main():
     # Récupérer la largeur du navigateur
     browser_width = st_javascript("window.innerWidth")
 
-    # Créer deux colonnes
-    ratios = [0.25, 0.75]
-    _, col2_w = list(map(lambda x: x * browser_width, ratios))
-    col1, col2 = st.columns(ratios)  # Ajuster les proportions des colonnes si nécessaire
-    
-    # Colonne 1 : Afficher les points sélectionnés
-    with col1:
         # Téléchargement de l'image
-        st.subheader("Upload a graph to digitize", divider= "rainbow")
-        uploaded_file = st.file_uploader(
-            "Upload a graph capture",
-            type=["png", "jpg", "jpeg"], 
-            label_visibility="collapsed"
+    st.subheader("Upload a graph to digitize", divider= "rainbow")
+    uploaded_file = st.file_uploader(
+        "Upload a graph capture",
+        type=["png", "jpg", "jpeg"], 
+        label_visibility="collapsed"
+        )
+
+    image = None
+    if uploaded_file:
+        try:
+        # Charger l'image et récupérer ses dimensions
+            image = Image.open(uploaded_file).convert("RGBA")
+            original_width, original_height = image.size
+            # Calculer la nouvelle hauteur de l'image pour garder les proportions
+            resized_width = int(col2_w) - 10
+            resized_height = int(resized_width * original_height / original_width)
+            st.success("Image téléchargée avec succès.")
+        except Exception as e:
+            st.error(f"Échec du téléchargement ou du traitement de l'image : {e}")
+    else:
+        st.warning("Veuillez télécharger une image ou vérifier le chemin de l'image par défaut.")
+
+    if image:
+        try:
+            # Dimensions fixes pour le canevas
+            resized_width, resized_height = 800, 600
+            st.write(f"Dimensions du canevas : {resized_width} x {resized_height}")
+            # Afficher le canevas avec l'image
+            canvas_result = st_canvas(
+                #fill_color="rgba(255, 165, 0, 0.3)",  # Couleur de remplissage
+                stroke_width=8,  # Épaisseur des lignes
+                stroke_color="#FF4B4B",  # Couleur des lignes
+                background_image=image,  # Image de fond
+                update_streamlit=True,
+                height=resized_height,
+                width=resized_width,
+                #drawing_mode="point",  # Mode point
+                #point_display_radius=4,  # Rayon des points
+                key="canvas",
             )
-
-        image = None
-        if uploaded_file:
-            try:
-            # Charger l'image et récupérer ses dimensions
-                image = Image.open(uploaded_file).convert("RGBA")
-                original_width, original_height = image.size
-                # Calculer la nouvelle hauteur de l'image pour garder les proportions
-                resized_width = int(col2_w) - 10
-                resized_height = int(resized_width * original_height / original_width)
-                st.success("Image téléchargée avec succès.")
-            except Exception as e:
-                st.error(f"Échec du téléchargement ou du traitement de l'image : {e}")
-        else:
-            st.warning("Veuillez télécharger une image ou vérifier le chemin de l'image par défaut.")
-
-        if image:
-            try:
-                # Dimensions fixes pour le canevas
-                resized_width, resized_height = 800, 600
-                st.write(f"Dimensions du canevas : {resized_width} x {resized_height}")
-                with col2:
-                    # Afficher le canevas avec l'image
-                    canvas_result = st_canvas(
-                        #fill_color="rgba(255, 165, 0, 0.3)",  # Couleur de remplissage
-                        stroke_width=8,  # Épaisseur des lignes
-                        stroke_color="#FF4B4B",  # Couleur des lignes
-                        background_image=image,  # Image de fond
-                        update_streamlit=True,
-                        height=resized_height,
-                        width=resized_width,
-                        #drawing_mode="point",  # Mode point
-                        #point_display_radius=4,  # Rayon des points
-                        key="canvas",
-                    )
-            except Exception as e:
-                st.error(f"Une erreur s'est produite lors de l'affichage du canevas : {e}")
+        except Exception as e:
+            st.error(f"Une erreur s'est produite lors de l'affichage du canevas : {e}")
 
     #     if uploaded_file:
 
