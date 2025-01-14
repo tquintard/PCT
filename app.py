70016bb9a1020e91bfcef65efcff13d3e1511ad7
tquintard <95873239+tquintard@users.noreply.github.com>
2025-01-14 11:50:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 from PIL import Image
 from streamlit_javascript import st_javascript
-from typing import List
+from typing import List, Tuple
 import pandas as pd
 
 from collections import defaultdict
@@ -11,7 +12,7 @@ TABS = ["Upload", "Calibration", "Curve points"]
 VAR_TO_CLEAR = ["cal_OK", "cal_pts"]
 
 @st.cache_data
-def load_n_resize_image(uploaded_file, width):
+def load_n_resize_image(uploaded_file: UploadedFile, width:float)->Image.Image:
     # Charger l'image et récupérer ses dimensions
     image = Image.open(uploaded_file).convert('L') # ouvrir l'image dans un format compresse
     original_width, original_height = image.size
@@ -20,32 +21,35 @@ def load_n_resize_image(uploaded_file, width):
     resized_height = int(resized_width * original_height / original_width)
     return image.resize((resized_width, resized_height))
 
-def clear_stss(variables:List[str]):
-    for var in variables:
-        if st.session_state.get(var):
-            del st.session_state[var]
+@st.cache_data
+def do_cal(points: List[dict])->Tuple[Tuple[float, float], Tuple[float, float]]: 
+    abs_origin = (points[0]['left'], points[0]['top'])
+    abs_axis = (points[1]['left'], points[2]['top'])
+    pxl = abs_axis[0] - abs_origin[0], abs_origin[1] - abs_axis[1]
+    return abs_origin, pxl
 
-def reset_cal():
-    st.session_state["cal_OK"] = False
+def create_columns(col2_ratio:float)->Tuple:
+    col2_width = st_javascript("window.innerWidth") * col2_ratio
+    col1, col2 = st.columns([1-col2_ratio, col2_ratio])
+    return col1, col2, col2_width
+
+def reset_cal(cv_res=None)->None:
+    #cases where calibration is reset
+    if cv_res == None :#if no image is uploaded
+        st.session_state["cal_OK"] = False
+    elif len(cv_res['objects']) < 3:#if no points are drawn on canvas
+        st.session_state["cal_OK"] = False
 
 def init_page ():
     # Configurer les paramètres de la page
     st.set_page_config(
-        page_title="Graph Digitizer",  # Titre de la page
+        page_title="Graph Digitiser",  # Titre de la page
         layout="wide",  # Mode large
         initial_sidebar_state="collapsed",  # Barre latérale déployée
     )
-    
-    # Titre de l'application
-    st.title("Graph Digitizer")  
-    
-    # Récupérer la largeur du navigateur
-    browser_width = st_javascript("window.innerWidth")
-    
-    # Créer deux colonnes
-    ratios = [0.3, 0.7]
-    st.session_state["col_width"] = list(map(lambda x: x * browser_width, ratios))
-    st.session_state["columns"] = st.columns(ratios)  # Ajuster les proportions des colonnes si nécessaire
+
+    # App title
+    st.title("Graph Digitiser") 
 
 def update_canva(image):
     return st_canvas(
@@ -56,71 +60,57 @@ def update_canva(image):
         height=image.height,
         width=image.width,
         drawing_mode="freedraw",
-        key="set_canvas",
+        #key="set_canvas",
     )
 
-def main():
+def def_cal_pts()-> List[Tuple[float, float]]:
+    cal_pts = []
+    for point in ['origin', 'axis']:
+        st.write(f"###### {point.title()} absolute coordinates")
+        cola, colb = st.columns(2)
+        with cola:
+            x = st.number_input(f"X-axis absolute {point}", min_value=-100_000_000, value=0 if point =='origin' else None, on_change= lambda: reset_cal())
+        with colb:
+            y = st.number_input(f"Y-axis absolute {point}", min_value=-100_000_000, value=0 if point =='origin' else None, on_change= lambda: reset_cal())           
+        cal_pts.append((x, y))
+    return cal_pts
 
+def main():
     #Variable initialisation
-    stss = st.session_state
-    col1, col2 = stss["columns"]
-    _, col2_w = stss["col_width"]
+    stss = st.session_state 
     
-    
+    # Columns creation
+    col1, col2, col2_w = create_columns(col2_ratio=0.7)
+
     with col2:
         st.subheader("Graph", divider='rainbow')  
     with col1:
-        
-        # Téléchargement de l'image
+        ## File upload ##
         st.subheader("Upload", divider='rainbow')
         uploaded_file = st.file_uploader("Upload a graph to digitize", type=["png", "jpg", "jpeg"], label_visibility = "collapsed")
-        if uploaded_file:
-            image = load_n_resize_image(uploaded_file, col2_w)
+        if uploaded_file:        
             with col2:
-                cv_res = update_canva(image)
+                image = load_n_resize_image(uploaded_file, col2_w)
+                cv_res = update_canva(image).json_data
 
+            ## Calibration ##
             st.subheader("Calibration", divider='rainbow')
-            if cv_res.json_data == None :
-                stss["cal_OK"] = False
-            elif len(cv_res.json_data['objects']) < 3:
-                stss["cal_OK"] = False
+            #check if the canva needs to be reseted
+            reset_cal(cv_res)
 
-            if not stss.get("cal_pts"):
-                stss["cal_pts"] = {"origin": {"abs": [0, 0], "rel": [0, 0]}, "axis": {"abs": [0, 0], "rel": [None, None]}}
-                stss["pxl"] = []
-            rel_origin, rel_axis = stss["cal_pts"]['origin']['rel'], stss["cal_pts"]['axis']['rel']
-
-            #Calibrate origin
-            st.write("###### Origin's absolute coordinates")
-            cola, colb = st.columns(2)
-            with cola:
-                rel_origin[0] = st.number_input("X-axis absolute origin", min_value=-100_000_000, value=rel_origin[0], key="rel_origin_x",on_change= lambda: reset_cal())
-            with colb:
-                rel_origin[1] = st.number_input("Y-axis absolute origin", min_value=-100_000_000, value=rel_origin[1], key="rel_origin_y",on_change= lambda: reset_cal())           
-            
-            #Calibrate Axis
-            st.write("###### Axis absolute coordinates")
-            cola, colb = st.columns(2)
-            with cola:
-                rel_axis[0] = st.number_input("X-axis absolute value", min_value=-100_000_000, value=rel_axis[0], key="rel_axis_x",on_change= lambda: reset_cal())
-            with colb:
-                rel_axis[1] = st.number_input("Y-axis absolute value", min_value=-100_000_000, value=rel_axis[1], key="rel_axis_y",on_change= lambda: reset_cal())
-            stss["cal_pts"]['origin']['rel'], stss["cal_pts"]['axis']['rel'] = rel_origin, rel_axis
+            #Show the relative calibration points to be filled by the user
+            rel_origin, rel_axis = def_cal_pts()
             
             if not stss.get("cal_OK"):
-                points = cv_res.json_data['objects'][:3] if cv_res.json_data != None else []
+                points = cv_res['objects'][:3] if cv_res != None else []
                 if len(points) == 3:
                     if None in (rel_origin + rel_axis):
                         st.error("At least one calibration point is not filled")
                     else:
-                        abs_origin = (points[0]['left'], points[0]['top'])
-                        abs_axis = (points[1]['left'], points[2]['top'])
-                        stss["cal_pts"]['origin']['abs'], stss["cal_pts"]['axis']['abs'] = abs_origin, abs_axis
-                        pxl = abs_axis[0] - abs_origin[0], abs_origin[1] - abs_axis[1]
-                        if pxl[0]*pxl[1] == 0:
+                        stss['abs_origin'], stss['pxl'] = do_cal(points)
+                        if any(pxl for pxl in stss["pxl"]) == 0:
                             st.error("Please select point on X-axis and Y-axis different from origin")
                         else:
-                            stss["pxl"] = pxl
                             stss["cal_OK"] = True
                             st.success("Calibration completed.")
                 elif len(points) < 3:
@@ -130,42 +120,40 @@ def main():
                             - A point on the X-axis
                             - A point on the Y-axis
                             """)
+            else:
+                ## Curve point ##
+                st.subheader("Curve points", divider='rainbow')
+                abs_origin, pxl = stss["abs_origin"], stss["pxl"]
+                data_points = [(point["left"], point["top"]) for point in cv_res["objects"][3:]]
+                if data_points == []:
+                    st.write("#### Select curve points on graph")
+                else:
+                    subcol1, subcol2 = st.columns(2, vertical_alignment = 'center')
+                    with subcol1:
+                        sign_fig = st.slider("Number of significant digits", min_value=0, max_value=5, value=0, step=1)
+                        
+                    with subcol2:
+                        if st.button("Export data"):
+                            st.write("Hello")
+                        
+
+                    calibrated_points=defaultdict(Tuple[float, float])
+                    for idx, point in enumerate(data_points):
+                        real_x = rel_origin[0] + (rel_axis[0] - rel_origin[0]) * (point[0] - abs_origin[0]) / pxl[0]
+                        real_y = rel_origin[1] + (rel_axis[1] - rel_origin[1]) * (abs_origin[1] - point[1]) / pxl[1]
+                        calibrated_points[f'Pt {idx+1}'] = real_x, real_y
+
+                    df = pd.DataFrame(calibrated_points, index=["X", "Y"])
+
+                    df_styled = df.style.format(f"{{:.{sign_fig}f}}", subset=df.select_dtypes(include="number").columns)
+
+                    st.dataframe(df_styled, use_container_width = True) 
 
         else:
-            clear_stss(VAR_TO_CLEAR)
-            with col2:
-                st.warning("Please upload a graph to digitize.")
+            reset_cal()
+            with col2: st.warning("Please upload a graph to be digitised")
 
-        
-        if stss.get("cal_OK"):
-            st.subheader("Curve points", divider='rainbow')
-            rel_origin, rel_axis = stss["cal_pts"]['origin']['rel'], stss["cal_pts"]['axis']['rel']
-            abs_origin, abs_axis, pxl = stss["cal_pts"]['origin']['abs'], stss["cal_pts"]['axis']['abs'], stss["pxl"]
-            data_points = [(point["left"], point["top"]) for point in cv_res.json_data["objects"][3:]]
-            if data_points == []:
-                st.write("#### Select curve points on graph")
-            else:
-                subcol1, subcol2 = st.columns(2, vertical_alignment = 'center')
-                with subcol1:
-                    sign_fig = st.slider("Number of significant digits", min_value=0, max_value=5, value=0, step=1)
-                    
-                with subcol2:
-                    if st.button("Export data"):
-                        st.write("Hello")
-                    
-
-                calibrated_points=defaultdict(list)
-                for idx, point in enumerate(data_points):
-                    real_x = rel_origin[0] + (rel_axis[0] - rel_origin[0]) * (point[0] - abs_origin[0]) / pxl[0]
-                    real_y = rel_origin[1] + (rel_axis[1] - rel_origin[1]) * (abs_origin[1] - point[1]) / pxl[1]
-                    calibrated_points[f'Pt {idx+1}'] = [real_x, real_y]               
-                
-                
-                df = pd.DataFrame(calibrated_points, index=["X", "Y"])
-
-                df_styled = df.style.format(f"{{:.{sign_fig}f}}", subset=df.select_dtypes(include="number").columns)
-
-                st.dataframe(df_styled, use_container_width = True) 
+ 
 
 if __name__ == "__main__":
     init_page()
